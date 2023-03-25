f1760d5a3c2333681ddef9463bcafb3341f05cce
Taylor Mitchell <taylor.j.mitchell@gmail.com>
2023-03-25 19:06:02 -0400

    wip

--- components/MobileKeyboardBar.tsx
+++ components/MobileKeyboardBar.tsx
@@ -1,9 +1,31 @@
 // components/MobileKeyboardBar.js
-import React from "react";
+import { useRef, useState, useEffect } from "react";
 
-const MobileKeyboardBar = ({ viewPortHeight = 0, height = 50 }) => {
+const MobileKeyboardBar = ({ height = 50 }) => {
+  const [bar, setBar] = useState(null);
+  useEffect(() => {
+    const handleResize = () => {
+      if (!bar) return;
+      const show = window.visualViewport.height / window.screen.availHeight < 0.7;
+      if (show) {
+        bar.style.display = "flex";
+        const top = window.visualViewport.pageTop + window.visualViewport.height - height;
+        bar.style.top = `${top}px`;
+      } else {
+        bar.style.display = "none";
+      }
+    };
+    window.visualViewport.addEventListener("resize", handleResize);
+    window.visualViewport.addEventListener("scroll", handleResize);
+    window.addEventListener("click", handleResize);
+    return () => {
+      window.visualViewport.removeEventListener("resize", handleResize);
+      window.visualViewport.removeEventListener("scroll", handleResize);
+      window.removeEventListener("click", handleResize);
+    };
+  }, [bar]);
   return (
-    <div className="mobile-keyboard-bar" style={{ top: viewPortHeight - height, height }}>
+    <div ref={setBar} className="mobile-keyboard-bar" style={{ height }}>
       <button>Button 1</button>
       <button>Button 2</button>
       <button>Button 3</button>
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,30 +4,15 @@ import MobileKeyboardBar from "../components/MobileKeyboardBar";
 import "../components/MobileKeyboardBar.css";
 
 function App({ Component, pageProps }) {
-  const [isKeyboardOpen, setIsKeyboardOpen] = useState(true);
-  const [keyboardFromTop, setKeyboardFromTop] = useState(0);
-  const handleResize = () => {
-    setIsKeyboardOpen(window.visualViewport.height / window.screen.availHeight < 0.7);
-    setKeyboardFromTop(window.visualViewport.pageTop + window.visualViewport.height);
-  };
-  useEffect(() => {
-    const update = () => requestAnimationFrame(handleResize);
-    window.visualViewport.addEventListener("resize", update);
-    window.visualViewport.addEventListener("scroll", update);
-    return () => {
-      window.visualViewport.removeEventListener("resize", update);
-      window.visualViewport.removeEventListener("scroll", update);
-    };
-  }, []);
-
   return (
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
       <Component {...pageProps} />
-      {JSON.stringify({ isKeyboardOpen, keyboardFromTop })}
-      {isKeyboardOpen && <MobileKeyboardBar viewPortHeight={keyboardFromTop} />}
+      {/* {JSON.stringify({ isKeyboardOpen, keyboardFromTop })} */}
+      {/* {isKeyboardOpen && <MobileKeyboardBar viewPortHeight={keyboardFromTop} />} */}
+      <MobileKeyboardBar />
     </>
   );
 }
