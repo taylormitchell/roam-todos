4186de83ce4f135d19edf4a111d27f47ef1bd429
Taylor Mitchell <taylor.j.mitchell@gmail.com>
2023-03-28 21:54:35 -0400

    Add header and footer

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,27 +1,26 @@
 import Head from "next/head";
+import { useRouter } from "next/router";
 import "../components/MobileKeyboardBar.css";
 import "../styles/globals.css";
 
 function App({ Component, pageProps }) {
+  const router = useRouter();
   return (
-    <>
+    <div id="app">
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
       </Head>
-      <div
-        style={{
-          position: "fixed",
-          top: 0,
-          left: 0,
-          height: "100%",
-          width: "100%",
-          overflowY: "scroll",
-          overflowX: "hidden",
-        }}
-      >
+      <header>
+        <button onClick={() => router.back()}>Back</button>
+      </header>
+      <main>
         <Component {...pageProps} />
-      </div>
-    </>
+      </main>
+      <footer>
+        <button onClick={() => router.push("/")}>Home</button>
+        <button onClick={() => router.push("/roam")}>Search</button>
+      </footer>
+    </div>
   );
 }
 
